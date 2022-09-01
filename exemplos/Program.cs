1210ac8f6ea3c3f40d5fa9f97188c752b755fa25
Antonio Ergori <antonio.ergori@unesp.br>
2022-09-01 12:20:55 -0300

    Segunda alteração de Érgori

--- exemplos/Program.cs
+++ exemplos/Program.cs
@@ -3,6 +3,7 @@
     private static void Main(string[] args)
     {
 	//alteração realizada por Érgori
+	//outra alteração realizada por Érgori
         Console.WriteLine("Digite seu ano de nascimento:");
         int anoN = int.Parse(Console.ReadLine());
         Console.WriteLine($"Ano de nascimento = " + (anoN));
